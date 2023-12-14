1a96160cb52b4b09bb32ae8cfdbedf2f68a3d53c
baryon2 <opthami@gmail.com>
2023-12-14 11:25:22 +0530

    Fix cosmos kit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -58,9 +58,9 @@ function CreateCosmosApp({ Component, pageProps }: AppProps) {
                 projectId: "a8510432ebb71e6948cfd6cde54b70f7",
                 relayUrl: "wss://relay.walletconnect.org",
                 metadata: {
-                  name: "CosmosKit Template",
-                  description: "CosmosKit dapp template",
-                  url: "https://docs.cosmoskit.com/",
+                  name: "Bad Kids Shop",
+                  description: "Bad Kids Shop",
+                  url: window.location.origin,
                   icons: [],
                 },
               },
