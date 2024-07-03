604970699e349edd28b1135e057c37e94b365f86
henrio123 <henriots33@gmail.com>
2024-07-03 15:12:11 +0300

    snapshot

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,13 +2,12 @@ import Image from "next/image";
 import { FaBars, FaXTwitter } from "react-icons/fa6";
 import { MdOutlineClose } from "react-icons/md";
 import CelestineSlothsSociety from "../public/celestine-sloths-society.png";
-
 import { useChain } from "@cosmos-kit/react-lite";
 import dynamic from "next/dynamic";
 import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { WalletButton } from "../components/WalletButton";
 import Text from "./Text";
-
+import { useRouter } from 'next/router';
 import StargazeLogo from "../public/stargaze-logo.svg";
 
 export const ElementsContainerDynamic = dynamic(
@@ -37,6 +36,7 @@ export function Header({
   openEmbeddedWalletModal: Function;
   setIsElementsModalOpen: Dispatch<SetStateAction<boolean>>;
 }) {
+  const router = useRouter();
   const { address, chain } = useChain("stargaze");
 
   const [balance, setBalance] = useState(null);
@@ -50,7 +50,7 @@ export function Header({
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <section className="flex flex-wrap gap-x-3  gap-y-3 fixed backdrop-blur-md bg-[#212121DE]  items-center justify-between px-10 py-4 top-0 left-0 right-0 z-10">
+    <section className="flex flex-wrap gap-x-3 gap-y-3 fixed backdrop-blur-md bg-[#212121DE] items-center justify-between px-10 py-4 top-0 left-0 right-0 z-10">
       <Image
         src={CelestineSlothsSociety}
         alt="celestine-sloths"
@@ -65,8 +65,19 @@ export function Header({
         )}
       </button>
 
-      <div id="nav-menu" className={` md:block ${isMenuOpen ? "" : "hidden"}`}>
+      <div id="nav-menu" className={`md:block ${isMenuOpen ? "" : "hidden"}`}>
         <div className="flex flex-row gap-3 flex-wrap">
+          <button
+            onClick={() => {
+              router.push('/snapshot');
+            }}
+            className="flex gap-2 items-center h-10 justify-between border border-white-100 rounded-3xl px-5 py-2"
+          >
+            <Text size="sm" color="text-white-100 font-bold">
+              Snapshot
+            </Text>
+          </button>
+
           <button
             onClick={() =>
               window.open("https://twitter.com/CelestineSloths", "_blank")
